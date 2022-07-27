37b0658a212df5f15754337d963fde2d0b9cdb07
AndriySavchuk <savu45321@gmail.com>
2022-07-28 00:09:32 +0300

    lesson4

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { userRouter } = require('./routes');
+mongoose.connect('mongodb://localhost:27017/dec');
 
+const userRouter = require('./routes/user.router');
 
 const app = express();
 app.use(express.json());
@@ -9,11 +11,21 @@ app.use(express.json());
 app.use('/users', userRouter);
 
 app.use('*', (req, res) => {
-    res.status(404).json('page not found');
-})
+    res.status(404).json('Route not found');
+});
+
+app.use((err, req, res, next) => {
+    res
+        .status(err.status || 500)
+        .json({
+            error: err.message || 'Unknown Error',
+            code: err.status || 500
+        });
+});
+
 app.listen(5000, () => {
-    console.log("Hii");
-})
+    console.log('Server listen 5000')
+});
 
 
 
