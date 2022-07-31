b3cf1df9ca7d97b405319ec7ce7f74694469427a
AndriySavchuk <savu45321@gmail.com>
2022-08-01 01:11:20 +0300

    lesson8

--- configs/configs.js
+++ configs/configs.js
@@ -5,8 +5,12 @@ module.exports = {
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || 'ats',
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || 'rts',
 
-    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL || '321@gmail.com',
-    NO_REPLY_EMAIL_PASSWORD: process.env.NO_REPLY_EMAIL_PASSWORD || '12345',
+    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL || '5321@gmail.com',
+    NO_REPLY_EMAIL_PASSWORD: process.env.NO_REPLY_EMAIL_PASSWORD || 'ct',
 
-    FRONTEND_URL: process.env.FRONTEND_URL || 'https://google.com'
+    FRONTEND_URL: process.env.FRONTEND_URL || 'https://google.com',
+
+    TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID || 'AC6930ccf3cf8a5cda39f70216c4e17166',
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN || '55415db8d5120d0175edd8e6737ea935',
+    TWILIO_NUMBER: process.env.TWILIO_NUMBER || '+18454079532',
 };
